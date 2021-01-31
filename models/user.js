bf6641499d53fb3b21016370dbfa19775b39ac57
Benny Borunda <blznk128@yahoo.com>
2021-01-30 21:17:25 -0700

    heroku

---
+++ models/user.js
@@ -0,0 +1,10 @@
+module.exports = (sequelize, DataTypes) => {
+    let User = sequelize.define("User", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    })
+
+    return User
+}
