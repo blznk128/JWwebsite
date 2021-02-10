9a6373d33af2663091c2045041fa3c2526376a26
Benny Borunda <blznk128@yahoo.com>
2021-02-09 17:26:10 -0700

    changed file structure to basic web page

--- routes/htmlRoute.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-// var isAuthenticated = require("../config/middleware/isAuthenticated");
-module.exports = function(app) {
-    app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/index.html"))
-    });
-}
